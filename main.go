1ca16c72b61d5c2fccca4ffa532b735557904241
Spencer Peterson <spencerjpeterson@gmail.com>
2018-10-29 00:28:40 -0700

    spooky webserver!

--- main.go
+++ main.go
@@ -1,84 +1,72 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
+	"html/template"
 	"math/rand"
-	"os"
+	"net/http"
 	"time"
 
 	"github.com/spencer-p/geneticdj/sexpr"
 )
 
-const (
-	// Some samples that sound good.
-	RYG    = "(+ (& 127 (| (/ (* t (% (^ (>> t 12) (- (>> t 12) 2)) 11)) 4) (>> t 13))) (& (/ (* t (& (i [51 54 51 54 52 54 56 57] (& (>> t 13) 7)) 15)) 12) 128))"
-	VIZNUT = "(| (& (* t 5) (>> t 7)) (& (* t 3) (>> t 10)))"
+var (
+	templates = template.Must(template.ParseFiles("generate.html", "play.html",
+		"error.html", "index.html"))
+	generator = sexpr.Preset{
+		MaxDepth:      10,
+		NumRange:      [2]int{1, 10},
+		ArrayLenRange: [2]int{3, 7},
+		OddsBranch:    0.75,
+		OddsVar:       0.5,
+	}
 )
 
-var (
-	fGenerate   = flag.Bool("generate", false, "generate and play a function")
-	fCustomFunc = flag.String("play", "", "your own function to play")
-	fDuration   = flag.String("duration", "30s", "duration to play (30s, 1h, etc)")
-	fWriteFile  = flag.String("o", "", "write the audio to a file")
-)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", interesting_fns)
+}
+
+func generateHandler(w http.ResponseWriter, r *http.Request) {
+	fn := generator.Generate()
+	templates.ExecuteTemplate(w, "generate.html", fn.String())
+}
+
+func playHandler(w http.ResponseWriter, r *http.Request) {
+	fn_text := r.URL.Path[len("/play/"):]
+	templates.ExecuteTemplate(w, "play.html", fn_text)
+}
+
+func audioHandler(w http.ResponseWriter, r *http.Request) {
+	fn_text := r.URL.Path[len("/api/audio/"):]
+
+	// Parse the function
+	fn, err := sexpr.Parse(fn_text)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 Internal Server Error"))
+		return
+	}
+
+	// Write the audio
+	w.Header().Set("Content-Type", "audio/x-wav")
+	w.WriteHeader(http.StatusOK)
+
+	err = sexpr.WriteWav(fn, w, 30)
+	if err != nil {
+		// TODO The http response is actually not salvageable at this point.
+		// We've already written stuff to it. AFAIK there is no way to clear it.
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 Internal Server Error"))
+		return
+	}
+}
 
 func main() {
-	flag.Parse()
-	timeout, err := time.ParseDuration(*fDuration)
+	rand.Seed(time.Now().UnixNano())
 
-	var fn sexpr.Fn
-	if *fGenerate || *fCustomFunc != "" {
+	http.HandleFunc("/play/", playHandler)
+	http.HandleFunc("/api/generate/", generateHandler)
+	http.HandleFunc("/api/audio/", audioHandler)
+	http.HandleFunc("/", indexHandler)
 
-		// Create the function desired
-		switch {
-		case *fGenerate:
-			rand.Seed(time.Now().UnixNano())
-			fn = sexpr.Preset{
-				MaxDepth:      10,
-				NumRange:      [2]int{1, 10},
-				ArrayLenRange: [2]int{3, 7},
-				OddsBranch:    0.75,
-				OddsVar:       0.5,
-			}.Generate()
-		case *fCustomFunc != "":
-			var err error // to avoid shadowing fn.
-			fn, err = sexpr.Parse(*fCustomFunc)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-	} else {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	// Print it for the user to see
-	fmt.Println(fn)
-
-	if *fWriteFile != "" {
-		file, err := os.Create(*fWriteFile)
-		if err != nil {
-			fmt.Println("Could not open", *fWriteFile, "for writing:", err)
-			os.Exit(1)
-		}
-		defer file.Close()
-		err = sexpr.WriteWav(fn, file, int(timeout.Seconds()))
-		if err != nil {
-			fmt.Println("Could not write audio:", err)
-			os.Exit(1)
-		}
-	} else {
-		// Set up context
-		if err != nil {
-			fmt.Println("Invalid duration:", err)
-		}
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		defer cancel()
-
-		// Play it
-		play(ctx, fn)
-	}
+	http.ListenAndServe(":8080", nil)
 }
