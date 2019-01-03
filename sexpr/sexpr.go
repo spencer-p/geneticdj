90ac02627f67bbb4c7080c39a419aedf3b960256
Spencer Peterson <spencerjpeterson@gmail.com>
2019-01-02 16:36:08 -0800

    document sexpr.Fn interface

--- sexpr/sexpr.go
+++ sexpr/sexpr.go
@@ -11,7 +11,13 @@ import (
 // Fn is any s-expression function that can be evaluated. Atoms and other
 // operators implement this interface.
 type Fn interface {
+	// Eval evaluates the function with the given argument.
 	Eval(int) int
+
+	// String returns an s-expression representation of the function.
 	String() string
+
+	// WriteBytes writes the ASCII bytes that make up an s-expression
+	// representation of the function.
 	WriteBytes(io.Writer)
 }
